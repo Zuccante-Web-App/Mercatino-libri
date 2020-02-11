fd402d93a2fc5bb42414a60b995481c662884410
rickieTheHuman <rickiejp12@gmail.com>
2020-02-11 15:26:52 +0100

    DB

--- services/utenti/lib/db.js
+++ services/utenti/lib/db.js
@@ -6,7 +6,7 @@ class Database {
         this.connection = mysql.createConnection(config)
     }
 
-    connect() {
+    async connect() {
         return new Promise((resolve, reject)=> {
             this.connection.connect((err)=> {
                 if(err)
@@ -16,7 +16,7 @@ class Database {
         })
     }
 
-    query(sql) {
+    async query(sql)  {
         return new Promise((resolve, reject) => {
             this.connection.query(sql, (err, result) => {
                 if(err)
--- services/utenti/lib/index.js
+++ services/utenti/lib/index.js
@@ -3,7 +3,6 @@ let express = require('express')
 let bodyParser = require('body-parser')
 
 async function main() {
-
     let db = new Database({
         host: "localhost",
         user: "root",
